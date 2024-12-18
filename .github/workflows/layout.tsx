4e6b843a8adbecf9ca88a53168cb8e51d36a04d3
RezzkzDA <rblxdava@gmail.com>
2024-12-18 19:07:36 +0700

    Create layout.tsx

---
+++ .github/workflows/layout.tsx
@@ -0,0 +1,22 @@
+import './globals.css'
+import type { Metadata } from 'next'
+import { Inter } from 'next/font/google'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export const metadata: Metadata = {
+  title: 'Zanzoken - Simple Modern Minecraft Server',
+  description: 'Join CubeCraft, a simple and modern Minecraft server. Build, explore, and connect with players from around the world.',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  )
+}
